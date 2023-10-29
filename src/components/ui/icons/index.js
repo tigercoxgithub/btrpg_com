02d37869914e03e2776275870e3815e626e5618c
Tiger Cox <tiger@tigercox.com>
2023-10-29 22:19:41 +0000

    many changes to home page text

--- src/components/ui/icons/index.js
+++ src/components/ui/icons/index.js
@@ -1,1 +1,6 @@
 export { default as Tick } from "./tick.astro";
+export { default as Eagle } from "./eagle.astro";
+export { default as Mountain } from "./mountain.astro";
+export { default as Suncloud } from "./suncloud.astro";
+export { default as Rocket } from "./rocket.astro";
+export { default as Logo } from "./logo.astro";
